274ab8231cf90b5f2fdd5f5f803025f2f1d78f70
Shmuela <shmuela@500tech.com>
2016-10-29 18:26:22 +0300

    move app-root template to inline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+<h1>
+  {{title}}
+</h1>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'My Todos';
 }
