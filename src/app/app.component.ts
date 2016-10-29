4ed53eb66ab5ee016c6edbd4501f35eb5562830e
Shmuela <shmuela@500tech.com>
2016-10-29 19:54:05 +0300

    add todoList to app-root: data and template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,24 @@ import { Component } from '@angular/core';
     </h1>
     
     <todo-input></todo-input>
+    
+    <ul>
+      <li *ngFor="let item of todoList">
+        {{ item.title }}
+      </li>
+    </ul>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   private title: string = 'My Todos';
+
+  private todoList = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ];
 }
